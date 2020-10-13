addb4e6ced5e299f33ba7c9fc07837840a555777
soma_arc <somaarcr@gmail.com>
2020-10-13 20:33:14 +0900

    Add examples

--- src/vue/othersPanel.vue
+++ src/vue/othersPanel.vue
@@ -7,6 +7,7 @@
     </button>
     
     <b-dropdown-item @click="load(2)" aria-role="listitem">Example 1</b-dropdown-item>
+    <b-dropdown-item @click="load(3)" aria-role="listitem">Example 2</b-dropdown-item>
     <b-dropdown-item @click="load(0)" aria-role="listitem">Ayumu Nagamatsu</b-dropdown-item>
     <b-dropdown-item @click="load(1)" aria-role="listitem">Joel Castellanos</b-dropdown-item>
   </b-dropdown>
